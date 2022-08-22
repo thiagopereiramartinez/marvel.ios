b3f3c734848cafc32698d660e625cabca1dbc22d
Thiago P. Martinez <thiago.pereira.ti@gmail.com>
2022-08-22 11:33:55 -0400

    Initial Commit

---
+++ Marvel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel
+//
+//  Created by Thiago P. Martinez on 22/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Thiago P. Martinez on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
